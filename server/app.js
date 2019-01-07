f706e7448510ae8c153d49aeea1006c91b6ef2c8
Mouad MOUNACH <me.mounach@gmail.com>
2019-01-07 11:56:28 +0100

    async await / cascade on delete and update film/categorie

--- server/app.js
+++ server/app.js
@@ -16,13 +16,13 @@ const filmsRoutes = require('./api/routes/filmRoutes');
 const categoriesRoutes = require('./api/routes/categorieRoutes');
 
 // MongoDB atlas connection
-// mongoose.connect(
-//     'mongodb://mounach:' +
-//     process.env.MONGO_ATLAS_PASSWORD + 
-//     '@nostroflix-shard-00-00-butgu.mongodb.net:27017,nostroflix-shard-00-01-butgu.mongodb.net:27017,nostroflix-shard-00-02-butgu.mongodb.net:27017/test?ssl=true&replicaSet=nostroflix-shard-0&authSource=admin&retryWrites=true',
-//     {
-//         useNewUrlParser: true
-//     });
+ /*mongoose.connect(
+     'mongodb://mounach:' +
+     process.env.MONGO_ATLAS_PASSWORD + 
+     '@nostroflix-shard-00-00-butgu.mongodb.net:27017,nostroflix-shard-00-01-butgu.mongodb.net:27017,nostroflix-shard-00-02-butgu.mongodb.net:27017/test?ssl=true&replicaSet=nostroflix-shard-0&authSource=admin&retryWrites=true',
+     {
+         useNewUrlParser: true
+     });*/
 
 mongoose.connect(
     'mongodb://localhost/nostroflix',
